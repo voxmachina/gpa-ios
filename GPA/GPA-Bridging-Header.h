d08e18e91a123ce9980ab58936151a2062ff8998
Pedro Eugenio <voxmachina@gmail.com>
2015-11-19 21:20:10 +0100

    Added swift bridge and delegate for google sign in

---
+++ GPA/GPA-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  GPA-Bridging-Header.h
+//  GPA
+//
+//  Created by Pedro on 19/11/15.
+//  Copyright © 2015 Pedro. All rights reserved.
+//
+
+// [START bridging_header]
+#import <Google/SignIn.h>
+// [END bridging_header]
