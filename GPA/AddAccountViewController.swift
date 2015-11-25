8be84bb1f35d596cb5fe81377ac20b9ee0c3c9b6
Pedro Eugenio <voxmachina@gmail.com>
2015-11-25 16:49:17 +0100

    Google connect working

--- GPA/AddAccountViewController.swift
+++ GPA/AddAccountViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
+import AeroGearHttp
+import AeroGearOAuth2
+
 class AddAccountViewController: UIViewController {
+    
+    var http: Http!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Let's register for settings update notification
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleSettingsChangedNotification",
+            name: NSUserDefaultsDidChangeNotification, object: nil)
+        self.http = Http()
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func handleSettingsChangedNotification() {
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        let clear = userDefaults.boolForKey("clearShootKeychain")
+        
+        if clear {
+            print("clearing keychain")
+            let kc = KeychainWrap()
+            kc.resetKeychain()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +46,55 @@ class AddAccountViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func connectToGoogle(sender: AnyObject) {
+        print("Perform connect with Google")
+        
+        let googleConfig = GoogleConfig(
+            clientId: "475540245735-uij4cg5s9ea6pfbt3mkrqrr6ps9l4emn.apps.googleusercontent.com",
+            scopes:["https://www.googleapis.com/auth/calendar"],
+            accountId: "dummy")
+        // If you want to use embedded web view uncomment
+        googleConfig.isWebView = true
+        
+        // Workaround issue on Keychain https://forums.developer.apple.com/message/23323
+        let gdModule =  OAuth2Module(config: googleConfig)
+        
+        //let gdModule = AccountManager.addGoogleAccount(googleConfig)
+        self.http.authzModule = gdModule
+        
+        gdModule.requestAccess { (response:AnyObject?, error:NSError?) -> Void in
+            if (error != nil) {
+                self.presentAlert("Error", message: error!.localizedDescription)
+            } else {
+                self.performAuthRequest("https://www.googleapis.com/calendar/v3/users/me/calendarList", parameters: nil)
+            }
+        }
+        
+    }
+    
+    func performAuthRequest(url: String, parameters: [String: AnyObject]?) {
+//        self.http.POST(url, parameters: parameters, completionHandler: {(response, error) in
+//            if (error != nil) {
+//                self.presentAlert("Error", message: error!.localizedDescription)
+//            } else {
+//                self.presentAlert("Success", message: "Successfully uploaded!")
+//            }
+//        })
+        self.http.GET(url, completionHandler: {(response, error) in
+            if (error != nil) {
+                self.presentAlert("Error", message: error!.localizedDescription)
+            } else {
+                print(response)
+                self.presentAlert("Success", message: "Google account added!")
+            }
+        })
+    }
+    
+    func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'GPA' do
+  pod 'AeroGearOAuth2'
+end
+
+target 'GPATests' do
+
+end
+
+target 'GPAUITests' do
+
+end
+
